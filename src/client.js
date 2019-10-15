a0524dac630a0b28ac78d36b67d14c6daade6120
Adam Sobotka <vorcigernix@gmail.com>
2019-10-15 20:08:40 +0200

    GA

--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 import * as sapper from '@sapper/app';
+import { googleAnalytics } from './anal.js';
 
 sapper.start({
 	target: document.querySelector('#sapper')
 });
+googleAnalytics('UA-134288088-2');
